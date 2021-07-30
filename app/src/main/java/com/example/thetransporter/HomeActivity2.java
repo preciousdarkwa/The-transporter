0eb5e59c38c2f91f26143357a65aaa61bb6de72d
Pdark <preciousdarkwa@gmail.com>
2021-07-30 16:43:24 +0000

    THE TRANSPORTER UI

--- app/src/main/java/com/example/thetransporter/HomeActivity2.java
+++ app/src/main/java/com/example/thetransporter/HomeActivity2.java
@@ -2,13 +2,31 @@ package com.example.thetransporter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity2 extends AppCompatActivity {
+    Button btnLogout;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home2);
+
+        btnLogout =findViewById(R.id.logout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToMain = new Intent(HomeActivity2.this, MainActivity.class);
+                startActivity(intToMain);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/thetransporter/MainActivity.java
+++ app/src/main/java/com/example/thetransporter/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.thetransporter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,11 +11,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    public EditText emailId,password;
+    EditText emailId,password;
     Button btnsignup;
     TextView tvsignin;
     FirebaseAuth mFirebaseAuth;
@@ -27,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         emailId = findViewById(R.id.editTextTextEmailAddress);
         password = findViewById(R.id.editTextTextPassword2);
         btnsignup = findViewById(R.id.button2);
+        tvsignin = findViewById(R.id.textView2);
         btnsignup.setOnClickListener(view -> {
             String email = emailId.getText() .toString();
             String pwd = password.getText().toString();
@@ -41,6 +47,31 @@ public class MainActivity extends AppCompatActivity {
             else if(email.isEmpty() && pwd.isEmpty()){
                 Toast.makeText(MainActivity.this, "Fields are empty!",Toast.LENGTH_SHORT).show();
             }
+            else if(!email.isEmpty() && pwd.isEmpty()){
+                mFirebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Sign Up unsuccessful! Please Try Again",Toast.LENGTH_SHORT).show();
+
+                        }
+                        else{
+                            startActivity(new Intent(MainActivity.this, HomeActivity2.class));
+                        }
+                    }
+                });
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Error occured!", Toast.LENGTH_SHORT).show();
+
+            }
+            });
+            tvsignin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent i = new Intent(MainActivity.this, LoginActivity2.class);
+                    startActivity(i);
+                }
             });
         };
     }
